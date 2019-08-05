e73e610fa0c5edccaa1d1bcc5668a900f469b457
Luce62006 <luce62006@gmail.com>
2019-08-05 17:02:12 +0200

    lab

--- babel.config.js
+++ babel.config.js
@@ -1,19 +1,12 @@
 module.exports = {
   presets: [
-    ['@babel/preset-env',
-      {
-        targets: {
-          node: 'current',
-        },
+    ['@babel/env', {
+      targets: {
+        node: 'current',
+        firefox: '60',
+        chrome: '67',
+        safari: '11.1',
       },
-    ],
-  ],
-  plugins: [
-    [
-      '@babel/plugin-proposal-pipeline-operator',
-      {
-        proposal: 'minimal',
-      },
-    ],
+    }],
   ],
 };
--- src/games/progression.js
+++ src/games/progression.js
@@ -33,13 +33,11 @@ const gameProgression = () => {
     const difD = Math.floor(Math.random() * (maxD - minD + 1)) + minD;
     const nSign = Math.floor(Math.random() * (maxN - minN + 1)) + minN;
     const answer = signA1 + difD * (nSign - 1);
-    const question = (c, y) => {
-      const c = signA1;
-      const y = difD;
+    const question = (signA1, difD) => {
       let result = ' ';
       let currChar = ' ';
       for (let n = 1; n <= 10; n += 1) {
-        currChar = c + y * (n - 1);
+        currChar = signA1 + difD * (n - 1);
         if (currChar === answer) {
           currChar = '..';
         }
