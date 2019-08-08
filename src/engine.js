1c5b8abd3797f437355e155907bbcee72884b973
Luce62006 <luce62006@gmail.com>
2019-08-08 17:40:19 +0200

    lab

---
+++ src/engine.js
@@ -0,0 +1,40 @@
+import readlineSync from 'readline-sync';
+import getName from '.';
+
+const roundsNumber = 3;
+
+const engine = (description, getQuestion, getCorrectAnswer) => {
+  // 1-й этап - вывод общеигрового приветствия
+  console.log('Welcome to the Brain Games!');
+  console.log();
+
+  // 2-й этап - вывод описания игры
+  console.log(description);
+  console.log();
+
+  // 3-й этап - запрос имени у пользователя
+  const name = getName();
+
+  // 4-й этап - приветствие пользователя
+  console.log(`Hello, ${name}!`);
+  console.log();
+
+  // 5-й этап - играем раунды
+  for (let i = 1; i <= roundsNumber; i += 1) {
+    const question = getQuestion();
+    console.log(`Question: ${question}`);
+    const answer = readlineSync.question('Your answer: ');
+    const correctAnswer = getCorrectAnswer(question);
+    if (answer === correctAnswer) {
+      console.log('Correct!');
+      console.log();
+    } else {
+      console.log(`"${answer}" is wrong answer! Correct answer is "${correctAnswer}".`);
+      console.log(`Let's try again, ${name}!`);
+      return;
+    }
+  }
+  console.log(`Congratulations, ${name}!`);
+};
+
+export default engine;
--- src/games/even.js
+++ src/games/even.js
@@ -1,42 +1,15 @@
-import readlineSync from 'readline-sync';
-import getName from '..';
+import engine from '../engine';
 
 const isEven = num => num % 2 === 0;
 
-const roundsNumber = 3;
 const min = 1;
 const max = 10;
 
 const evenGame = () => {
-  // 1-й этап - вывод общеигрового приветствия
-  console.log('Welcome to the Brain Games!');
-  console.log();
-  // 2-й этап - вывод описания игры
-  console.log('Answer "yes" if number even otherwise answer "no".');
-  console.log();
-  // 3-й этап - запрос имени у пользователя
-  const name = getName();
-  // 4-й этап - приветствие пользователя
-  console.log(`Hello, ${name}!`);
-  console.log();
-
-  // 5-й этап - играем раунды
-  for (let i = 1; i <= roundsNumber; i += 1) {
-    const question = Math.floor(Math.random() * (max - min + 1)) + min;
-    console.log(`Question: ${question}`);
-    const correctAnswer = isEven(question) ? 'yes' : 'no';
-    const answer = readlineSync.question('Your answer: ');
-
-    if (answer === correctAnswer) {
-      console.log('Correct!');
-      console.log();
-    } else {
-      console.log(`"${answer}" is wrong answer! Correct answer is "${correctAnswer}".`);
-      console.log(`Let's try again, ${name}!`);
-      return;
-    }
-  }
-  console.log(`Congratulations, ${name}!`);
+  const description = 'Answer "yes" if number even otherwise answer "no".';
+  const getQuestion = () => Math.floor(Math.random() * (max - min + 1)) + min;
+  const getCorrectAnswer = s => (isEven(s) ? 'yes' : 'no');
+  engine(description, getQuestion, getCorrectAnswer);
 };
 
 export default evenGame;
