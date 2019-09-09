5f7f4b1ad36ed2f560bd3e32ae99f8e1869a8508
Luce62006 <luce62006@gmail.com>
2019-09-09 18:31:18 +0200

    resolved shadow progression.js

--- src/games/progression.js
+++ src/games/progression.js
@@ -7,13 +7,8 @@ const getfirstMember = () => getRandomNum(1, 5);
 const getprogressionDifference = () => getRandomNum(2, 4);
 
 const gethiddenElementPosition = () => getRandomNum(lengthOfProgression - 1, 0);
-const getanswer = () => getfirstMember() + getprogressionDifference() * gethiddenElementPosition();
 
-const getQuestion = () => {
-  const firstMember = getfirstMember();
-   const progressionDifference = getprogressionDifference();
-  const hiddenElementPosition = gethiddenElementPosition();
-  const answer = firstMember + progressionDifference * hiddenElementPosition;
+const getQuestion = (lengthOfProgression, firstMember, progressionDifference, answer) => {
   
 let result = '';
   for (let n = 0; n <= lengthOfProgression; n += 1) {
@@ -22,7 +17,6 @@ let result = '';
       currChar = '..';
     }
     result = `${result} ${currChar}`;
-    
   }
   return result.trim();
 };
@@ -31,7 +25,7 @@ const description = ' What number is missing in the progression?';
 
 const getQuestionAndCorrectAnswer = () => {
  const firstMember = getfirstMember();
-   const progressionDifference = getprogressionDifference();
+  const progressionDifference = getprogressionDifference();
   const hiddenElementPosition = gethiddenElementPosition();
   const answer = firstMember + progressionDifference * hiddenElementPosition;
   const newQuestion = getQuestion(10, firstMember, progressionDifference, answer);
