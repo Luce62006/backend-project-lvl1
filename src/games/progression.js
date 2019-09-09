9e2b75ec9ccfdcb6bb6b03a799025391888af181
Luce62006 <luce62006@gmail.com>
2019-09-09 16:14:53 +0200

    resolved no shadow in progression.js

--- src/games/progression.js
+++ src/games/progression.js
@@ -9,11 +9,16 @@ const getprogressionDifference = () => getRandomNum(2, 4);
 const gethiddenElementPosition = () => getRandomNum(lengthOfProgression - 1, 0);
 const getanswer = () => getfirstMember() + getprogressionDifference() * gethiddenElementPosition();
 
-const getQuestion = (lengthOfProgression, getfirstMember, getprogressionDifference, getanswer) => {
+const getQuestion = () => {
+  const firstMember = getfirstMember();
+   const progressionDifference = getprogressionDifference();
+  const hiddenElementPosition = gethiddenElementPosition();
+  const answer = firstMember + progressionDifference * hiddenElementPosition;
+  
 let result = '';
   for (let n = 0; n <= lengthOfProgression; n += 1) {
-    let currChar = getfirstMember + getprogressionDifference * n;
-    if (currChar === getanswer) {
+    let currChar = firstMember + progressionDifference * n;
+    if (currChar === answer) {
       currChar = '..';
     }
     result = `${result} ${currChar}`;
