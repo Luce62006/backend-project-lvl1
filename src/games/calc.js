edf89d634f331608b352dedd2c54348e82583cfc
Luce62006 <luce62006@gmail.com>
2019-09-09 22:26:13 +0200

    fixed error calc.js

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,7 +1,9 @@
 import engine from '../engine';
 import getRandomNum from '../utils';
 
-const getSignofMathOperat = mathSign => (mathSign.charAt(getRandomNum(0, String(mathSign).length - 1)));
+const getSignofMathOperation = mathSign => {
+return mathSign.charAt(getRandomNum(0, String(mathSign).length - 1));
+};
 
 const getSign = (number1, sign, number2) => {
   let result;
@@ -24,7 +26,7 @@ const description = 'What is the result of the expression?';
 const getQuestionAndCorrectAnswer = () => {
   const number1 = getRandomNum(1, 30);
   const number2 = getRandomNum(1, 30);
-  const signofMathOperation = getSignofMathOperat('-+*');
+  const signofMathOperation = getSignofMathOperation('-+*');
   const correctAnswer = getSign(number1, signofMathOperation, number2);
   const question = `${number1} ${signofMathOperation} ${number2}`;
   const newCorrectAnswer = String(correctAnswer);
